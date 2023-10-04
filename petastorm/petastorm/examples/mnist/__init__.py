0960915e860822cf20488500214fa768c7afdc7c
Shiyue <shiyuehou1@gmail.com>
2023-10-04 15:01:56 +0000

    change petastorm/examples/mnist/__init__.py

--- petastorm/petastorm/examples/mnist/__init__.py
+++ petastorm/petastorm/examples/mnist/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEFAULT_MNIST_DATA_PATH = '/tmp/mnist'
+DEFAULT_MNIST_DATA_PATH = '/mnt/cephfs/minist_dataset'
