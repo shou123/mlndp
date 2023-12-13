675df7f7d32bed0e03204f56135920b4e623d5c5
Shiyue <shiyuehou1@gmail.com>
2023-12-13 21:57:39 +0000

    This cript implement batch fetch shuffle data

--- ML_example/mnist_bench_single.py
+++ ML_example/mnist_bench_single.py
@@ -10,8 +10,11 @@ import pyarrow.dataset as ds
 from torch.utils.data import Dataset
 import pyarrow as pa
 import time
+import numpy as np
 
-
+'''
+This cript implement batch fetch shuffle data
+'''
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -49,54 +52,119 @@ class CustomDataset(Dataset):
     def __len__(self):
         return self.record_count
 
-    def __getitem__(self, idx):
+    def __getitems__(self, idx):
         # sample = self.dataset.to_batches().take([idx])  # Use to_batches to get the record
-        self.total_comsume_time = 0
-        self.total_comsume_time1 = 0
+        self.comsume_time = 0
         
         
-        arrow_array = pa.array([idx])
-        start_time1 = time.time()
+        arrow_array = pa.array(idx,type=pa.int64())
+        start_time = time.time()
         # print(dataset.take(arrow_array).to_pandas())
         sample = self.dataset.take(arrow_array)  # Use to_batches to get the record
         end_time1 = time.time()
 
-        label = int(sample['label'].to_pandas())
-        start_time = time.time()
+        # labels = sample['label'].to_pandas()
+        labels = sample['label'].to_pandas()
+
         # image = torch.FloatTensor(sample['image'].to_pandas()).view((28,28)).unsqueeze(0)
-        image = torch.FloatTensor(sample['image'].to_pandas()).view((1,28,28))
-        end_time = time.time()
+        images = torch.FloatTensor(sample['image'].to_pandas()).view(32,1,28,28)
         
-        comsume_time = (end_time-start_time)
-        comsume_time1 = (end_time1-start_time1)
-
-        self.total_comsume_time+=comsume_time
-        self.total_comsume_time1+=comsume_time1
-
-        self.batch_count.append(comsume_time)
-        if len(self.batch_count) == 32:
-            print(f"each batch data reshape time: {self.total_comsume_time}")
-            print(f"each batch get data time: {self.total_comsume_time1}")
+        comsume_time = (end_time1-start_time)
+        with open('/home/yue21/mlndp/ML_example/result/take_time.txt', 'a') as take_time_save:
+            take_time_save.write(f"take_operation_time: {comsume_time} second\n")
         # image = torch.tensor(sample[0]['image'][0])
-        return image, label
 
+        # batch_data = [{'data': images[i], 'target': int(labels[i])} for i in range(len(idx))]
+        batch_data = [{'data': images[i], 'target': int(labels[i])} for i in range(len(idx))]
+
+
+        return batch_data
+
+
+# class CustomDataset(Dataset):
+#     def __init__(self, pyarrow_dataset, batch_size==32):
+#         self.record_batches = list(pyarrow_dataset.to_batches())
+#         self.batch_size = batch_size
+#         self.cache = {}  # Cache to store entire batches
+
+#     def __len__(self):
+#         return len(self.record_batches)
+
+#     def __getitem__(self, idx):
+#         if idx not in self.cache:
+#             # If data is not in the cache, compute and save the entire batch
+#             start_idx = idx * self.batch_size
+#             end_idx = (idx + 1) * self.batch_size
+
+#             image_data = self.record_batches[0]['image'].slice(start_idx, end_idx).to_pandas()
+#             # image_tensor = torch.FloatTensor(image_data).view(-1, 28, 28)
+#             image_tensor = torch.FloatTensor(image_data)
+
+#             label_data = self.record_batches[0]['label'].slice(start_idx, end_idx).to_pandas()
+
+#             # Save entire batch to cache
+#             self.cache[idx] = {'images': image_tensor, 'labels': label_data}
+
+#         # Return a single sample from the cached batch
+#         sample_idx = idx % self.batch_size
+#         reshaped_image = self.cache[idx]['images'][sample_idx].view(1, 28, 28)
+#         return reshaped_image, int(self.cache[idx]['labels'][sample_idx])
+
+# class CustomDataset(Dataset):
+#     def __init__(self, pyarrow_dataset):
+#         self.record_count = 0
+#         for _ in pyarrow_dataset.to_batches():
+#             self.record_count += len(_)  
+#         self.dataset = pyarrow_dataset# Count the records in the current batch
+
+#     def __len__(self):
+#         return self.record_count
+
+#     def __getitem__(self, idx):
+#         # sample = self.dataset.to_batches().take([idx])  # Use to_batches to get the record
+
+#         arrow_array = pa.array([idx])
+#         # print(dataset.take(arrow_array).to_pandas())
+#         sample = self.dataset.take(arrow_array)  # Use to_batches to get the record
+
+#         label = int(sample['label'].to_pandas())
+#         # image = torch.FloatTensor(sample['image'].to_pandas()).view((28,28)).unsqueeze(0)
+#         image = torch.FloatTensor(sample['image'].to_pandas()).view((1,28,28))
+#         # image = torch.tensor(sample[0]['image'][0])
+#         return image, label
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     # statixtic calculate
     total_data_processed = 0
-    start_time = time.time()
 
         # Open the files in append mode
     with open('/home/yue21/mlndp/ML_example/result/iops.txt', 'a') as iops_file, \
          open('/home/yue21/mlndp/ML_example/result/throughput.txt', 'a') as throughput_file, \
-         open('/home/yue21/mlndp/ML_example/result/latency.txt', 'a') as latency_file:
+         open('/home/yue21/mlndp/ML_example/result/latency.txt', 'a') as latency_file, \
+         open('/home/yue21/mlndp/ML_example/result/time_consume_with_ML.txt', 'a') as time_consume_with_ML:
 
-
-        for batch_idx, (data, target) in enumerate(train_loader):
+        batch_total_time = 0
+        batches_num = 0
+        # time stample, to do , one epoch. 
+        # for batch_idx, (data, target) in enumerate(train_loader):
+        for batch_idx, batch in enumerate(train_loader):
             start_time = time.time()
-            data, target = data.to(device), target.to(device)
+
+            #==================================for Pure ML =====================================
+            # image_data = batch[0]
+            # # Get label value for the current index
+            # label_value = batch[1]
+            # data, target = image_data.to(device), label_value.to(device)
+            #===================================================================================
+
+            #===================================For near data and non near data================================
+            batches_num = train_loader.dataset.record_count/train_loader.batch_size
+            data = batch['data']
+            label = batch['target']
+            data, target = data.to(device), label.to(device)
+            #====================================================================================================
             optimizer.zero_grad()
             output = model(data)
             loss = F.nll_loss(output, target)
@@ -109,10 +177,14 @@ def train(args, model, device, train_loader, optimizer, epoch):
             throughput = total_data_processed / elapsed_time # data per second
             latency = elapsed_time #time for processing for each batch
 
+            batch_total_time+=elapsed_time
+
             # Write the metrics to the files
             iops_file.write(f'IOPS: {iops:.2f} batch/s\n')
             throughput_file.write(f'Throughput: {throughput:.2f} data/s\n')
             latency_file.write(f'Batch_Latency: {latency:.6f} second\n')
+            time_consume_with_ML.write(f'train_time_consume: {latency:.6f} second\n')
+
 
             if batch_idx % args.log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -124,25 +196,32 @@ def train(args, model, device, train_loader, optimizer, epoch):
 
                 if args.dry_run:
                     break
+        # ========================for near data and non near data ====================================
+        ave_batch_time_consume = batch_total_time/batches_num
+        print(f"ave_batch_time_consume:{ave_batch_time_consume}")
+        #=============================================================================================
 
 
-def test(model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+# def test(model, device, test_loader):
+#     model.eval()
+#     test_loss = 0
+#     correct = 0
+#     with torch.no_grad():
+#         # for data, target in test_loader:
+#         for batch in enumerate(test_loader):
+#             data = batch['data']
+#             target = batch['target']
+#             data, target = data.to(device), target.to(device)
+#             output = model(data)
+#             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+#             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+#             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
+#     test_loss /= len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+#     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+#         test_loss, correct, len(test_loader.dataset),
+#         100. * correct / len(test_loader.dataset)))
 
 
 def main():
@@ -222,22 +301,28 @@ def main():
 
 
 #=============================Pure ML=================================================
-    dataset1 = datasets.MNIST('/mnt/cephfs/petastorm_dataset/pure_mnist_dataset', train=True, download=True,
-                       transform=transform)
-    dataset2 = datasets.MNIST('/mnt/cephfs/petastorm_dataset/pure_mnist_dataset', train=False,
-                       transform=transform)
+    # dataset1 = datasets.MNIST('/mnt/cephfs/petastorm_dataset/pure_mnist_dataset', train=True, download=True,
+    #                    transform=transform)
+    # dataset2 = datasets.MNIST('/mnt/cephfs/petastorm_dataset/pure_mnist_dataset', train=False,
+    #                    transform=transform)
 
-    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+    # train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    # test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 #=======================================================================================
 
 
     model = Net().to(device)
+    # model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False).to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+
     for epoch in range(1, args.epochs + 1):
+        start = time.time()
         train(args, model, device, train_loader , optimizer, epoch)
+        end = time.time()
+        epoch_time_comsume = end- start
+        print(f"epoch {epoch}: {epoch_time_comsume:.6f} second\n")
         test(model, device, test_loader)
         scheduler.step()
 
