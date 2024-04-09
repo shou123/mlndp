220d12c358b427b039f9e9910375ffccd9bcd21f
shiyue <shiyuehou1@gmail.com>
2024-04-09 01:40:49 +0000

    k_means

--- NDP_test/bench_k_means.py
+++ NDP_test/bench_k_means.py
@@ -10,11 +10,19 @@ import pyarrow.parquet as pq
 
 if __name__ == "__main__":
 
-    category = "pure_k_means"
-    # category = "skyhook"
-    # category = "non_skyhook"
 
-    n_clusters = 3 #the number of k_means
+    # category = 'pure_k_means'
+    # category = ["pure_k_means","skyhook","non_skyhook"]
+    # category = "skyhook"
+    category = "non_skyhook"
+
+    # n_clusters = [3,10,20,50] # the number of k_means
+    n_clusters = 50
+
+    # samples = [10000, 50000,100000,1000000] # the number of samples, setup 1w sample is a base. 
+    samples = 10000
+
+    rounds = 10
 
     if category == "skyhook" or category == "non_skyhook":
         if category == "skyhook":
@@ -24,9 +32,9 @@ if __name__ == "__main__":
 
         dataset = ds.dataset('/mnt/cephfs/k_means',format = format_)
 
-        query = 'SELECT Feature_1, Feature_2 FROM dataset LIMIT 1000'
+        query = f'SELECT Feature_1, Feature_2 FROM dataset LIMIT {samples}'
 
-        for i in range (100):
+        for i in range (rounds):
 
             s = time.time()
             conn = duckdb.connect()
@@ -40,9 +48,9 @@ if __name__ == "__main__":
             e = time.time()
 
             with open('NDP_test/data_collection/k_means_latency.txt', 'a') as file:
-                file.write(f"category: {category}, round: {i}, latency: {e - s}\n")
+                file.write(f"category: {category}, cluster: {n_clusters}, samples: {samples}, round: {i}, latency: {e - s}\n")
 
-            print(f"execute_k_means_rounds: {i}, execute_time: {e-s}")
+            print(f"category: {category}, cluster: {n_clusters}, samples: {samples}, round: {i}, latency: {e - s}")
 
     elif category == "pure_k_means":
 
@@ -52,16 +60,16 @@ if __name__ == "__main__":
             table = pq.read_table('/mnt/cephfs/k_means')
             df = table.to_pandas()
 
-            features = df[['Feature_1', 'Feature_2']].head(1000)
+            features = df[['Feature_1', 'Feature_2']].head(samples)
 
             kmeans = km(n_clusters, random_state=42,n_init=10)
             kmeans.fit(features)
             e = time.time()
 
             with open('NDP_test/data_collection/k_means_latency.txt', 'a') as file:
-                file.write(f"category: {category}, round: {i}, latency: {e - s}\n")
+                file.write(f"category: {category}, cluster: {n_clusters}, samples: {samples}, round: {i}, latency: {e - s}\n")
 
-            print(f"execute_k_means_rounds: {i}, execute_time: {e-s}")
+            print(f"category: {category}, cluster: {n_clusters}, samples: {samples}, round: {i}, latency: {e - s}")
 
             # plt.scatter(df['Feature_1'], df['Feature_2'], c=kmeans.labels_, cmap='viridis')
             # plt.xlabel('Feature_1')
